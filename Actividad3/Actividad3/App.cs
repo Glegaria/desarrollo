60e640ef4f6bb7dc94b57373336ab5e2a88a5e23
Glegaria <jglegaria@prodigy.net.mx>
2016-04-01 14:47:33 -0600

    Agregue boton con mi nombre

--- Actividad3/Actividad3/App.cs
+++ Actividad3/Actividad3/App.cs
@@ -3,18 +3,57 @@ using Xamarin.Forms;
 
 namespace Actividad3
 {
-	public class App
-	{
-		public static Page GetMainPage ()
-		{	
-			return new ContentPage { 
-				Content = new Label {
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
-				},
-			};
-		}
-	}
-}
+		class ButtonDemoPage : ContentPage
+					{
+						Label label;
+						
+						public ButtonDemoPage()
+						{
+							Label header = new Label
+							{
+								Text = "Button",
+								Font = Font.BoldSystemFontOfSize(50),
+								HorizontalOptions = LayoutOptions.Center
+							};
 
+							Button button = new Button
+							{
+								Text = "Presiona aqui!",
+								Font = Font.SystemFontOfSize(NamedSize.Large),
+								BorderWidth = 1,
+								HorizontalOptions = LayoutOptions.Center,
+								VerticalOptions = LayoutOptions.CenterAndExpand
+							};
+							button.Clicked += OnButtonClicked;
+
+							label = new Label
+							{
+								Text = "Usuario",
+								Font = Font.SystemFontOfSize(NamedSize.Large),
+								HorizontalOptions = LayoutOptions.Center,
+								VerticalOptions = LayoutOptions.CenterAndExpand
+							};
+
+							// Accomodate iPhone status bar.
+							this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+							// Build the page.
+							this.Content = new StackLayout
+							{
+								Children = 
+								{
+									header,
+									button,
+									label
+								}
+								};
+						}
+
+						void OnButtonClicked(object sender, EventArgs e)
+						{
+							label.Text = "JOSE GENARO LEGARIA MONSALVO";
+							//String.Format("{0} button click{1}",
+							//clickTotal, clickTotal == 1 ?  "" : "s");
+						}
+					}  
+}
